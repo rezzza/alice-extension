ffc2a6539062332c14b6a997004739519d89b109
Armand Abric <armand@verylastroom.com>
2014-04-16 11:25:50 +0200

    Change JSON detection method to allow more JSON synthax.

--- Tests/Units/AliceExtension/Alice/InlineFixtures.php
+++ Tests/Units/AliceExtension/Alice/InlineFixtures.php
@@ -7,64 +7,106 @@ use \Rezzza\AliceExtension\Alice\InlineFixtures as TestedClass;
 
 class InlineFixtures extends Units\Test
 {
+
     public function test_normalize_primitive_data()
     {
-        $testedClass = new TestedClass('aa', 'bbb', array('ccc'));
-
-        $resultA = $testedClass->normalize(array('abcd', ''));
-
-        $this
-                ->array($resultA)
-                ->contains('abcd')
-                ->contains('')
-                ->hasSize(2)
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array('abcd', ''))
+            )
+            ->array($result)
+            ->contains('abcd')
+            ->contains('')
+            ->hasSize(2)
         ;
 
-        $resultB = $testedClass->normalize(array(1));
-        $this
-                ->array($resultB)
-                ->contains(1)
-                ->hasSize(1)
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array(1))
+            )
+            ->array($result)
+            ->contains(1)
+            ->hasSize(1)
         ;
 
-        $resultC = $testedClass->normalize(array());
-        $this
-                ->array($resultC)
-                ->isEmpty()
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array())
+            )
+            ->array($result)
+            ->isEmpty()
         ;
     }
 
-    public function test_normalize_json_data()
+    public function test_normalize_yaml_notation()
     {
-        $testedClass = new TestedClass('aa', 'bbb', array('ccc'));
-
-        $resultA = $testedClass->normalize(array('["a","b","c"]'));
-        $this
-                ->array($resultA)
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array('[1,2,"c"]'))
+            )
+            ->array($result[0])
+            ->contains(1)
+            ->contains(2)
+            ->contains('c')
+            ->hasSize(3)
         ;
 
-        $this
-                ->array($resultA[0])
-                ->contains('a')
-                ->contains('b')
-                ->contains('c')
-                ->hasSize(3)
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array('{a: 1, b: 24,c: null}'))
+            )
+            ->array($result)
+            ->array($result[0])
+            ->hasKey('a')
+            ->hasKey('b')
+            ->contains(1)
+            ->contains("24")
+            ->contains(null)
+            ->hasSize(3)
         ;
 
-        $resultB = $testedClass->normalize(array('{"a": 1, "b": "24"}'));
-
-        $this
-                ->array($resultB)
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array('["a","b","c"]'))
+            )
+            ->array($result[0])
+            ->contains('a')
+            ->contains('b')
+            ->contains('c')
+            ->hasSize(3)
         ;
 
-        $this
-                ->array($resultB[0])
-                ->hasKey('a')
-                ->hasKey('b')
-                ->contains(1)
-                ->contains("24")
-                ->hasSize(2)
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->then(
+                $result = $testedClass->normalize(array('{"a": 1, "b": "24"}'))
+            )
+            ->array($result)
+            ->array($result[0])
+            ->hasKey('a')
+            ->hasKey('b')
+            ->contains(1)
+            ->contains("24")
+            ->hasSize(2)
         ;
+    }
+
+    public function test_normalize_json_syntax_error()
+    {
+        $this->given(
+                $testedClass = new TestedClass('aa', 'bbb', array('ccc'))
+            )->exception(function() use ($testedClass) {
+                $testedClass->normalize(array('{"dzqd"}'));
+            })
+            ->isInstanceOf('Symfony\Component\Yaml\Exception\ParseException')
+            ->message
+            ->contains('Unexpected characters');
     }
 
 }
