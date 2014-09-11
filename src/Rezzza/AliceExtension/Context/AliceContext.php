6573c3a2365c12f60cfb645d9eb1732114d7a634
Stéphane PY <py.stephane1@gmail.com>
2014-09-11 16:40:17 +0200

    In behat.yml you can provide many fixture files, choose your(s) defaults and launch fixture files one by one.

--- src/Rezzza/AliceExtension/Context/AliceContext.php
+++ src/Rezzza/AliceExtension/Context/AliceContext.php
@@ -21,6 +21,14 @@ class AliceContext extends BehatContext implements AliceAwareInterface
     }
 
     /**
+     * @Given /^I load "(?P<fixtureName>[^"]*)" fixtures$/
+     */
+    public function iLoadSpecificFixtureFile($fixtureName)
+    {
+        $this->executor->importFixtureKeyPath($fixtureName);
+    }
+
+    /**
      * @Given /^I load "(?P<className>[^"]*)" fixtures where column "(?P<columnKey>[^"]*)" is the key:$/
      */
     public function iLoadFixtures($className, $columnKey, TableNode $table)
